9e47063004386475a5b8f5da1d918a9e61231ae7
sameer-555 <sameerborkar6@gmail.com>
2019-12-17 22:21:11 +0530

    sameer:database changes in progress

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask , render_template , url_for, request
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.sqlite3"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database1.sqlite3"
 db = SQLAlchemy(app)
 
 class company_suggestion(db.Model):
@@ -10,48 +11,49 @@ class company_suggestion(db.Model):
 	company_name = db.Column(db.String(120), unique=True, nullable=False)
 	reason = db.Column(db.String (600), unique=False, nullable=False)
 	share_price = db.Column(db.Integer(), unique=False, nullable=True)
+	date = db.Column(db.DateTime, default = datetime.now())
 db.create_all()
 
-posts = [
-	{
-		'Stock_Price':'Stock Price',
-		'title': 'Company 1',
-		'content':'Short content',
-		'date_posted':'April 21,2019'
-	},
-	{
-		'Stock_Price':'Stock Price',
-		'title': 'Company 2',
-		'content':'Short content',
-		'date_posted':'April 21,2019'
-	},
-	{
-		'Stock_Price':'Stock Price',
-		'title': 'Company 66',
-		'content':'Short content',
-		'date_posted':'April 21,2019'
-	},
-	{
-		'Stock_Price':'Stock Price',
-		'title': 'Company 543',
-		'content':'Short content',
-		'date_posted':'April 21,2019'
-	}
-]
+
 
 @app.route("/")
 @app.route("/home")
 def home():
-	return render_template('home.html',posts=posts)
+	return render_template('home.html')
 
 @app.route("/company_invested/", methods = ["GET","POST"])
 def company_suggested():
 	if request.method == "GET":
-		print("---------------hello---------------")
 		return render_template('company_invested.html',title='Companies')
 
 	if request.method == "POST":
-		print("--------------in post-----------------")
+		if request.form["share_price"]:
+			print(request.form["company_name"])
+			com_sugg=company_suggestion(company_name = request.form["company_name"], reason = request.form["reason"],share_price = request.form["share_price"])
+		else:
+			com_sugg=company_suggestion(company_name = request.form["company_name"], reason = request.form["reason"])
+		try:
+			db.session.add(com_sugg)
+			db.session.commit()
+		except:
+			return "Sorry, there was issue while inserting the values."
+		li = company_suggestion.query.all()
+		dic = 	{
+		'Stock_Price':0,
+		'title': '',
+		'content':'',
+		'date_posted':0
+		}
+		post = []
+		for row in li:	
+			dic["Stock_Price"] = row.share_price	
+			dic["title"] = row.company_name
+			dic["reason"] = row.reason
+			dic["date_posted"] = row.date
+			print(dic)
+			post.append(dic)
+		print(post)
+		return render_template('home.html',posts=post)
 		
 
 if __name__ == "__main__":
